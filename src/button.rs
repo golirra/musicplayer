619f2c28b4696bfc3480175e1c6a4f688d9ec00f
Seamus Webb <webbseamus2@gmail.com>
2025-03-08 17:12:50 -0800

    implement slider, subscription
    subscription is a way to listen to events every second

--- src/button.rs
+++ src/button.rs
@@ -1,13 +1,17 @@
 #![allow(dead_code)]
 use std::fs::File;
 use std::io::BufReader;
+use std::time::Duration;
 
+
+use iced::{executor, time, Application };
+use iced::Subscription;
 use iced::{Fill, Element };
-use iced::widget::{button, column, container, row, text};
+use iced::widget::{button, column, container, progress_bar, row, slider, text};
 use iced::window;
+use crate::playback;
 
-use rodio::{Decoder, OutputStream, Sink};
-
+use rodio::{Decoder, OutputStream, Sink, Source};
 
 #[derive(Clone, Debug)]
 pub enum PlaybackAction {
@@ -18,11 +22,19 @@ pub enum PlaybackAction {
     Previous,
     Next,
     NextRand,
+    SliderChanged(f32),
+    UpdatePos(f32),
+    Tick,
+}
+
+pub struct State {
+    value: f32,
 }
 
 #[derive(Default)]
 pub struct PlaybackController {
-    value: u64,
+    value: f32,
+    song_pos: f32,
     sink: Option<Sink>,
     _stream: Option<OutputStream>,
 }
@@ -36,16 +48,27 @@ impl PlaybackController {
 
     pub fn new() -> Self {
         Self {
-            value: 0,
+            value: 0.0,
+            song_pos: 0.0,
             sink: None ,
             _stream: None,
             //button_state: button::State::new(),
         }
     }
-
+    
     pub fn view(&self) -> Element<PlaybackAction> {
         container(
             row![
+            slider(0.0..=100.0, self.value, PlaybackAction::SliderChanged),
+            progress_bar(0.0..=100.0, self.value), 
+            text(
+                match &self.sink {
+                    Some(x) => x.get_pos(),
+                    None => Duration::new(5, 0),
+                }.as_secs()
+            ),
+            container(
+                row![
                 button("Load audio").on_press(PlaybackAction::Load),
                 button("Stop").on_press(PlaybackAction::Stop),
                 button("Play").on_press(PlaybackAction::Play),
@@ -53,13 +76,19 @@ impl PlaybackController {
                 button("Previous").on_press(PlaybackAction::Previous),
                 button("Next").on_press(PlaybackAction::Next),
                 button("NextRand").on_press(PlaybackAction::NextRand),
-                text(self.value).size(20),
+                ]
+                .spacing(1)
+            )
             ]
-            .spacing(1)
         )
-        .center_x(Fill)
+            .center_x(Fill)
         .center_y(Fill)
         .into()
+    }
+
+    pub fn subscription(&self) -> Subscription<PlaybackAction> {
+        println!("Subscription started");
+        time::every(Duration::from_secs(1)).map(|_instant| PlaybackAction::Tick) // Update every second
     }
 
     pub fn update(&mut self, message: PlaybackAction) {
@@ -68,10 +97,22 @@ impl PlaybackController {
                 self.load_audio();
             },
             PlaybackAction::Play => {
-                self.play_audio();
+                if let Some(sink) = &self.sink {
+                    if sink.is_paused() {
+                        sink.play();
+                    } else {
+                        sink.pause();
+                    }
+                }
             },
-            PlaybackAction::Pause => {
-                self.pause_audio();
+            PlaybackAction::SliderChanged(value) => {
+                self.value = value;
+            },
+            PlaybackAction::UpdatePos(value) => {
+            },
+            PlaybackAction::Tick => {
+                self.update_song_pos();
+                println!("Tick");
             },
                 
             _ => {}, 
@@ -84,17 +125,33 @@ impl PlaybackController {
             let sink = Sink::try_new(&stream_handle).unwrap();
             let file = BufReader::new(File::open("song.mp3").unwrap());
             let source = Decoder::new(file).unwrap();
+            
 
+            /*
+            source.periodic_access(Duration::from_secs(1), |source|{
+                self.update_song_pos();
+            });
+            */
             sink.append(source);
+            sink.set_volume(0.05);
+            
 
             self._stream = Some(stream);
             self.sink = Some(sink);
+            
+        }
+    }
+
+    pub fn update_song_pos(&mut self) {
+        if let Some(sink) = &self.sink {
+            self.update(PlaybackAction::SliderChanged(sink.get_pos().as_secs_f32()));
         }
     }
 
     pub fn play_audio(&mut self) {
         if let Some(sink) = &self.sink {
             sink.play();
+            self.value = 51.0;
         }
     }
 
@@ -104,7 +161,6 @@ impl PlaybackController {
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 mod button;
+mod playback;
 use crate::button::PlaybackController;
+use iced::Subscription;
+
 fn main() -> iced::Result {
    
-    iced::run("Play me some music", PlaybackController::update, PlaybackController::view)
+    //iced::run("Play me some music", playback::update, playback::view)
+    //iced::run("Play me some music", PlaybackController::update, PlaybackController::view)
+    iced::application("Music", PlaybackController::update, PlaybackController::view)
+        .subscription(PlaybackController::subscription)
+        .run()
+
 }
