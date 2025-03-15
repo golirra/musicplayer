38b0ed585ba27a94f7475869c242d5c8c4d1811e
Seamus Webb <webbseamus2@gmail.com>
2025-03-14 21:10:00 -0700

    add get_song_path to pull from a sql db

--- src/app/state/db/database.rs
+++ src/app/state/db/database.rs
@@ -1,1 +1,7 @@
+use rusqlite::{Connection, Result};
+pub fn get_song_path(conn: &Connection, song_name: &str) -> Result<String> {
+    let mut stmt = conn.prepare("SELECT path FROM files WHERE name = ? AND attribs = 32")?;
+    let song_path: String = stmt.query_row([song_name], |row| row.get(0))?;
+    Ok(song_path)
+}
 
